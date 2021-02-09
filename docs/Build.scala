78f741a4995b99a2a939357b52332f1c7f570891
Anton Sviridov <keynmol@gmail.com>
2021-02-09 10:16:39 +0000

    add CNAME

--- docs/Build.scala
+++ docs/Build.scala
@@ -16,13 +16,26 @@
 
 package mdoc.effect
 
+import java.time.LocalDate
+
+import subatomic._
 import subatomic.builders._
 import subatomic.builders.librarysite._
 
 object Build extends LibrarySite.App {
+  override def extra(site: Site[LibrarySite.Doc]) = {
+    site
+      .addCopyOf(SiteRoot / "CNAME", os.pwd / "docs" / "assets" / "CNAME")
+  }
+
+  val currentYear = LocalDate.now().getYear()
+
   def config = LibrarySite(
     contentRoot = os.pwd / "docs" / "pages",
     name = "Mdoc helper for Cats Effect",
+    githubUrl = Some("https://github.com/indoorvivants/mdoc-effect"),
+    assetsFilter = _.baseName != "CNAME",
+    copyright = Some(s"© 2021-$currentYear Anton Sviridov"),
     assetsRoot = Some(os.pwd / "docs" / "assets"),
     highlightJS = HighlightJS.default.copy(theme = "monokai-sublime")
   )
